9665785934dd7de3842ef476e3d90bb1db348d42
Parabeac-Nest <admin@parabeac.com>
2022-03-09 17:26:21 +0000

    Auto-generated commit by Parabeac Nest

---
+++ lib/main_widgetbook.g.dart
@@ -0,0 +1,34 @@
+    import 'package:widgetbook/widgetbook.dart';
+    import 'package:flutter/material.dart';
+    
+
+    void main() {
+      runApp(const MyApp());
+    }
+
+    class MyApp extends StatelessWidget {
+      const MyApp({Key? key}) : super(key: key);
+
+      @override
+      Widget build(BuildContext context){
+        return Widgetbook(
+          themes: [
+            WidgetbookTheme(name: 'Light', data: ThemeData.light()),
+          ],
+          devices: const [
+            Apple.iPhone11ProMax,
+            Samsung.s10,
+          ],
+          categories: [
+                  WidgetbookCategory(
+        name: 'Parabeac-Generated',
+        
+        
+      )
+    ,
+          ],
+          appInfo: AppInfo(name: 'MyApp'),
+        );
+      }
+    }
+    
